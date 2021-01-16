375e06cf2c89dc72e5ab0f78c6a7fe544aebfcf1
anirbandas <anirbandas08@gmail.com>
2021-01-16 15:09:38 -0500

    SQL practice

--- BasicSQL/basicQuery.sql
+++ BasicSQL/basicQuery.sql
@@ -12,3 +12,53 @@ select name from city where countrycode='USA' and population>120000;
 -- where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 select city,name from station where LAT_N>0 and LONG_W<0;
+
+--Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+-- For example, if there are three records in the table with CITY values 'New York', 'New York', 'Bengalaru',
+-- there are 2 different city names: 'New York' and 'Bengalaru'. The query returns , because
+
+select count(city)-count(distinct(city)) from station;
+
+--Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths
+-- (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one
+-- that comes first when ordered alphabetically.
+
+select city, length(city)
+from (
+        select
+        city,
+        ROW_NUMBER() OVER ( ORDER BY LENGTH( CITY ) ASC,  CITY ) shortest_rn,
+        ROW_NUMBER() OVER ( ORDER BY LENGTH( CITY ) DESC, CITY ) longest_rn
+        from station
+    )
+where shortest_rn = 1
+or longest_rn = 1;
+
+--Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+select distinct(city)
+from station
+where
+city like ('a%')
+or
+city like ('e%')
+or
+city like ('i%')
+or
+city like ('o%')
+or
+city like ('u%');
+
+--Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+select distinct(city)
+from station
+where
+city like ('%a')
+or
+city like ('%e')
+or
+city like ('%i')
+or
+city like ('%o')
+or
+city like ('%u');
+
